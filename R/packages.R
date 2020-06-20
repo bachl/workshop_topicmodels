039d19b477d01153a3f9ddedcc6c67e668c3f445
Marko <marko.bachl@gmail.com>
2020-06-20 11:30:14 +0200

    Initial commit

---
+++ R/packages.R
@@ -0,0 +1,9 @@
+## ---- packages
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, stm, stminsights, tidytext)
+theme_set(theme_bw()) # ggplot theme
+
+tibble(package = c("R", sort(pacman::p_loaded()))) %>% 
+  mutate(version = map_chr(package, ~as.character(pacman::p_version(package = .x)))) %>% 
+  knitr::kable()
---
+++ README.md
@@ -0,0 +1,3 @@
+--- Work in progress ---
+Material zum virtuellen Mini-Workshop Topic Models am IJK.
+* R Skripte sind in `~/R`
